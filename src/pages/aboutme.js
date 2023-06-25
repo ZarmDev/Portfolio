66c77d60137a37b407bc1552c28a61ce4439d10c
ZarmDev <80705328+ZarmDev@users.noreply.github.com>
2023-06-25 10:34:27 -0400

    Responsive design fixes

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -5,7 +5,7 @@ export default function AboutMe() {
     <section id="about">
       <div>
         <h1 class="heading">About Me</h1>
-        <h1 style={{fontSize: '20ch'}}>Hi, I'm Danial Armiyaev</h1>
+        <h1 id="hello">Hi, I'm Danial Armiyaev</h1>
         <pre>
             I'm a student at Brooklyn Tech who loves coding, soccer and gaming!
             I enjoy exploring, traveling and trying out new things.
